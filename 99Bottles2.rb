14f0fb05aebe8b39bfbf3edf0ae1abc518803147
Grace Park <parkgra3@gmail.com>
2018-06-26 09:13:48 +0000

    Done.

---
+++ 99Bottles2.rb
@@ -0,0 +1,19 @@
+
+
+      i = 99
+
+      while i <= 99 and i > 2 do
+
+        puts "#{i} bottles of beer on the wall. #{i} bottles of beer."
+
+      i = i - 1
+
+        puts "Take one down, pass it around. #{i} bottles of beer on the wall."
+
+        puts "2 bottles of beer on the wall, 2 bottles of beer.
+        Take one down and pass it around, 1 bottle of beer on the wall."
+        
+        puts "Take one down and pass it around, no more bottles of beer on the wall. 
+        No more bottles of beer on the wall, no more bottles of beer. 
+        Go to the store and buy some more, 99 bottles of beer on the wall."
+      end
